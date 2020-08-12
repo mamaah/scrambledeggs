41e835a82b1b0ef5241ca1652b9459a07f73bbaf
Rahael Akwe <rahaelakwe06@gmail.com>
2020-08-11 19:50:13 -0500

    snack

--- snack.py
+++ snack.py
@@ -2,14 +2,18 @@
 fav_snack = 'meatpie'
 print(fav_snack)
 print(fav_snack * 100)
+#ask friwnd's fav snack
 freinds_fav_snack = input('please enter your fav snack:')
 freinds_fav_snack = 'gato'
 print(fav_snack, freinds_fav_snack)
 print(fav_snack * 100, freinds_fav_snack * 100)
+#grocery list
 items = ['ngon', 'tiok', 'pombe', 'pah', 'nyam', 'gato']
 grocery = items
 print(grocery)
+#testing if friend's snack is in the list
 items.index('gato')
+#fast swapping
 temp = fav_snack
 fav_snack = freinds_fav_snack
 freinds_fav_snack = temp
