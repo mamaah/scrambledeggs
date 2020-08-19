d2a5e8c6b99a28e2f16c4e4779a298efae0231d5
Rahael Akwe <rahaelakwe06@gmail.com>
2020-08-18 20:11:15 -0500

    function assignment

---
+++ function_assignment.py
@@ -0,0 +1,35 @@
+#function assignment 
+mylist = ['beans', 'corn', 'palmoil', 'fish', 'njanga']
+print('beans')
+print('corn')
+print('palmoil')
+print('fish')
+print('njanga')
+9.42 + 5.57 + 3.25 + 13.40 + 7.50
+9.42 + 5.57 + 3.25 + 13.40 + (7.50 * 5)
+print('beans', 'corn', 'palmoil', 'fish', 'njanga')
+def all_the_snacks(snack):
+    print( snack * 100)
+def another_all_the_snacks('beans', 100)
+def grocery_list(item):
+    g_list = ('beans', 'corn', 'njanga')
+    if item in g_list:
+        return True
+    else:
+        return False
+def price_matcher():
+    grocery_list = ('beans', 'corn', 'palmoil', 'fish', 'njanga')
+    prices = (9.42, 5.57, 3.25, 13.40, 7.50)
+    print(random.choice(grocery_list) + " ", random.choice(prices))
+def all_the_snacks(snack, spacer=',', number=100):
+    print((snack + spacer) * 100)
+def april_fool_swapper():
+    my_color = input('please enter your favorite color: ')
+    neighbors_color = input('please enter your neighbors favorite color: ')
+    print('my favorite color is ' + my_color)
+    print('my neighbors favorite color is ' + neighbors_color)
+    print('my favorite color is ' + neighbors_color)
+    print('my neighbors favorite color is ' + my_color)
+
+    
+
